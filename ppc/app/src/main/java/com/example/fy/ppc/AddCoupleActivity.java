110e5f87bd94bbdd6e45aa532da70ad1dcf36e1c
faouziath <yessoufouf@gmail.com>
2015-12-19 18:15:21 -0500

    mise a jour

--- ppc/app/src/main/java/com/example/fy/ppc/AddCoupleActivity.java
+++ ppc/app/src/main/java/com/example/fy/ppc/AddCoupleActivity.java
@@ -1,25 +1,18 @@
 package com.example.fy.ppc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
-<<<<<<< HEAD:ppc/app/src/main/java/com/example/fy/ppc/AddCoupleActivity.java
-public class AddCoupleActivity extends AppCompatActivity {
-=======
 import client.ClientActivity;
 import common.Message;
 
+public class AddCoupleActivity extends ClientActivity {
 
-public class StartedActivity extends ClientActivity {
->>>>>>> 748b5f009d0616cda2b229f3aa216bdb5e404ef6:ppc/app/src/main/java/com/example/fy/ppc/StartedActivity.java
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:ppc/app/src/main/java/com/example/fy/ppc/AddCoupleActivity.java
         setContentView(R.layout.activity_addcouple);
-=======
-        setContentView(R.layout.activity_started);
         int flag =0;
 
         if(flag == 0){
@@ -46,6 +39,5 @@ public class StartedActivity extends ClientActivity {
         if(message.getSubject() == Message.Subject.P2P)
         System.out.println(message.getBody()+ " from " + message.getFrom());
 
->>>>>>> 748b5f009d0616cda2b229f3aa216bdb5e404ef6:ppc/app/src/main/java/com/example/fy/ppc/StartedActivity.java
     }
 }
